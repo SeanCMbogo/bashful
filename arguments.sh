43bd8cdfe2972184c3ed9503ee4828f3a9f6122d
Sean C M <Sean.C.Mbogo@gmail.com>
2022-04-17 14:27:42 -0400

    Update arguments.sh

--- arguments.sh
+++ arguments.sh
@@ -4,4 +4,7 @@ echo "File name is "$0 # holdds the current script name
 echo $3 # $3 contains the fourth argument passed in 
 Data=$5 # Assigns the sixth argument to a variable 
 echo "A $Data costs justs $6." 
+# The variable $# holds the number of arguments passed to the script
+# The variable $@ holds a space delimited string of all arguments passed to the script
 echo $# 
+echo $@ 
