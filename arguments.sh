9d3bd04d35aa73899b4144f9c7287b6de517fd75
Sean C M <Sean.C.Mbogo@gmail.com>
2022-04-17 14:25:37 -0400

    Adding in arguments.sh`

---
+++ arguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# example usage bash arguments.sh apple 5 banana 8 "Fruit Basket" 15
+echo "File name is "$0 # holdds the current script name 
+echo $3 # $3 contains the fourth argument passed in 
+Data=$5 # Assigns the sixth argument to a variable 
+echo "A $Data costs justs $6." 
+echo $# 
